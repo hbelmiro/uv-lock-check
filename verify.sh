2f9d66392c6ed53e574d496261376e8e4dd42123
Helber Belmiro <helber.belmiro@gmail.com>
2025-07-11 19:01:38 -0300

    Fix

--- verify.sh
+++ verify.sh
@@ -8,12 +8,6 @@ pwd
 echo "Running sync command: $SYNC_COMMAND"
 eval "$SYNC_COMMAND"
 
-git status --short
-
-if ! git diff --exit-code > /dev/null; then
-    echo "❌ Some files are out of sync (see git diff above)"
-    git --no-pager diff
-    exit 1
-fi
+git diff --exit-code
 
 echo "✅ All files are in sync"
